eff17198c6d6a7a8120334b4655e58290bc04010
NikolayIT <admin@nikolay.it>
2014-05-01 18:43:32 +0300

    Work on the UI design of the system

--- Source/BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
+++ Source/BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
@@ -13,18 +13,17 @@
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.themes.unify.app.css",
+                      "~/Content/bootstrap.themes.unify.css",
+                      "~/Content/site.blog.css",
+                      "~/Content/bootstrap.themes.unify.red.css",
+                      "~/Content/site.common.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
