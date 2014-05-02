6c07645c992a892ab3d954cc511028a719f783d0
NikolayIT <admin@nikolay.it>
2014-05-02 18:36:08 +0300

    Implemented migration between the old database and the new one

---
+++ Source/BlogSystem/Tools/BlogSystem.Tools.MigrateOldWebsite/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BlogSystem.Tools.MigrateOldWebsite
+{
+    using System.Data.Entity;
+
+    using BlogSystem.Data;
+    using BlogSystem.Data.Migrations;
+    using BlogSystem.Data.Models;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, DefaultMigrationConfiguration>());
+
+            var oldDatabase = new nikolayitEntities();
+            var newDatabase = new ApplicationDbContext();
+
+            foreach (var post in oldDatabase.posts)
+            {
+                var newPost = new BlogPost
+                                  {
+                                      Content = post.content,
+                                      CreatedOn = post.published,
+                                      PreserveCreatedOn = true,
+                                      MetaDescription = post.meta_description,
+                                      MetaKeywords = post.meta_keywords,
+                                      ShortContent = post.content_short,
+                                      ModifiedOn = post.last_update,
+                                      SubTitle = post.subtitle,
+                                      Title = post.title,
+                                  };
+
+                newDatabase.BlogPosts.Add(newPost);
+            }
+
+            newDatabase.SaveChanges();
+        }
+    }
+}
