84ae31d4d92c499c0521f11d3031e4fa23f1be25
ivaylokenov <ivaylo.kenov@gmail.com>
2014-05-22 14:54:50 +0300

    Structure map action filter registry implemented

--- Source/BlogSystem/BlogSystem.Web/Infrastructure/Filters/RouteValuesInViewData.cs
+++ Source/BlogSystem/BlogSystem.Web/Infrastructure/Filters/RouteValuesInViewData.cs
@@ -2,7 +2,7 @@
 {
     using System.Web.Mvc;
 
-    public class PassRouteValuesToViewDataAttribute : ActionFilterAttribute
+    public class PassRouteValuesToViewDataAttribute : ActionFilterAttribute, IActionFilter
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
---
+++ Source/BlogSystem/BlogSystem.Web/Infrastructure/Registries/ActionFilterRegistry.cs
@@ -0,0 +1,23 @@
+﻿namespace BlogSystem.Web.Infrastructure.Registries
+{
+    using StructureMap;
+    using StructureMap.Configuration.DSL;
+    using System;
+    using System.Web.Mvc;
+
+    public class ActionFilterRegistry : Registry
+    {
+        public ActionFilterRegistry(Func<IContainer> containerFactory)
+        {
+            For<IFilterProvider>().Use(
+                new StructureMapFilterProvider(containerFactory));
+
+            Policies.SetAllProperties(
+                x => x.Matching(p => 
+                    p.DeclaringType.IsAssignableFrom(typeof(ActionFilterAttribute)) &&
+                    p.DeclaringType.Namespace.StartsWith("BlogSystem") &&
+                    !p.PropertyType.IsPrimitive &&
+                    p.PropertyType != typeof(string)));
+        }
+    }
+}
