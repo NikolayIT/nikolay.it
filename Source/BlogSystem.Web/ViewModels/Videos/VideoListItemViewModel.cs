7969b51fd8bbbc9b4ce83f737237caf0c25b8913
Nikolay Kostov <admin@nikolay.it>
2015-05-23 18:38:33 +0300

    Added view models for video list

---
+++ Source/BlogSystem.Web/ViewModels/Videos/VideoListItemViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace BlogSystem.Web.ViewModels.Videos
+{
+    using BlogSystem.Data.Models;
+
+    public class VideoListItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string VideoId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public VideoProvider Provider { get; set; }
+
+        public string VideoUrl
+        {
+            get
+            {
+                if (this.Provider == VideoProvider.YouTube)
+                {
+                    return string.Format("https://www.youtube.com/watch?v={0}", this.VideoId);
+                }
+                else
+                {
+                    return this.VideoId; // TODO: Or implement video URL in Video model when provider is not YouTube
+                }
+            }
+        }
+
+        public string ThumbnailUrl
+        {
+            get
+            {
+                if (this.Provider == VideoProvider.YouTube)
+                {
+                    return string.Format("https://i.ytimg.com/vi/{0}/mqdefault.jpg", this.VideoId);
+                }
+                else
+                {
+                    return this.VideoId; // TODO: Implement thumbnail URL in Video model when provider is not YouTube
+                }
+            }
+        }
+    }
+}
---
+++ Source/BlogSystem.Web/ViewModels/Videos/VideoListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogSystem.Web.ViewModels.Videos
+{
+    using System.Collections.Generic;
+
+    public class VideoListViewModel
+    {
+        public int Page { get; set; }
+
+        public int Pages { get; set; }
+
+        public IEnumerable<VideoListItemViewModel> Videos { get; set; }
+    }
+}
