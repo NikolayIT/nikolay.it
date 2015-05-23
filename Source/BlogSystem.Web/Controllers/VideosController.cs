cec2499da1af50dbaed3e18dd9b05f9d2701f853
Nikolay Kostov <admin@nikolay.it>
2015-05-23 14:30:26 +0300

    Started working on videos listing

---
+++ Source/BlogSystem.Web/Controllers/VideosController.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
