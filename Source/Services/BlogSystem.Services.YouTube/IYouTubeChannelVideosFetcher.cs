9ebe6ff14b06eed4ec096fed4d3035a8ebf32b8f
Nikolay Kostov <admin@nikolay.it>
2015-06-18 00:43:33 +0300

    Improvements in recent videos

--- Source/Services/BlogSystem.Services.YouTube/IYouTubeChannelVideosFetcher.cs
+++ Source/Services/BlogSystem.Services.YouTube/IYouTubeChannelVideosFetcher.cs
@@ -7,7 +7,10 @@
 
     public interface IYouTubeChannelVideosFetcher
     {
-        IEnumerable<Snippet> GetLatestVideosFromChannel(string channelPlaylistId, Func<Item, bool> predicate);
+        IEnumerable<Snippet> GetLatestVideosFromChannel(
+            string channelPlaylistId,
+            int maxResults,
+            Func<Item, bool> predicate);
 
         IEnumerable<Snippet> GetAllVideosFromChannel(string channelPlaylistId, Func<Item, bool> predicate);
     }
--- Source/Services/BlogSystem.Services.YouTube/YouTubeChannelVideosFetcher.cs
+++ Source/Services/BlogSystem.Services.YouTube/YouTubeChannelVideosFetcher.cs
@@ -13,7 +13,7 @@
     public class YouTubeChannelVideosFetcher : IYouTubeChannelVideosFetcher
     {
         private const string UrlFormat =
-            "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={1}&key={0}&maxResults=50&pageToken={2}";
+            "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={1}&key={0}&maxResults={2}&pageToken={3}";
 
         private readonly string apiKey;
 
@@ -30,7 +30,7 @@
 
             do
             {
-                var jsonText = client.DownloadString(string.Format(UrlFormat, this.apiKey, channelPlaylistId, nextPage));
+                var jsonText = client.DownloadString(string.Format(UrlFormat, this.apiKey, channelPlaylistId, 50, nextPage));
                 var obj = JsonConvert.DeserializeObject<RootObject>(jsonText);
                 items.AddRange(obj.Items.Where(predicate).Select(x => x.Snippet));
                 nextPage = obj.NextPageToken;
@@ -40,12 +40,12 @@
             return items;
         }
 
-        public IEnumerable<Snippet> GetLatestVideosFromChannel(string channelPlaylistId, Func<Item, bool> predicate)
+        public IEnumerable<Snippet> GetLatestVideosFromChannel(string channelPlaylistId, int maxResults, Func<Item, bool> predicate)
         {
             var items = new List<Snippet>();
             var client = new WebClient { Encoding = Encoding.UTF8 };
 
-            var jsonText = client.DownloadString(string.Format(UrlFormat, this.apiKey, channelPlaylistId, string.Empty));
+            var jsonText = client.DownloadString(string.Format(UrlFormat, this.apiKey, channelPlaylistId, maxResults, string.Empty));
             var obj = JsonConvert.DeserializeObject<RootObject>(jsonText);
             items.AddRange(obj.Items.Where(predicate).Select(x => x.Snippet));
 
