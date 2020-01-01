280e23b2c1d6187c6f53e1195fbc61dea765caee
Nikolay Kostov <admin@nikolay.it>
2020-01-01 15:57:01 +0200

    Add missing images and google tracking code

--- src/Web/BlogSystem.Web/Startup.cs
+++ src/Web/BlogSystem.Web/Startup.cs
@@ -72,11 +72,7 @@
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-                if (env.IsDevelopment())
-                {
-                    dbContext.Database.Migrate();
-                }
+                dbContext.Database.Migrate();
 
                 new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
             }
